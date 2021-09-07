a11dde30a42532c15b31b1423eda095d38878226
pherapont <pherapont@yandex.ru>
2021-09-07 10:37:30 +0700

    Add project files.

---
+++ OpticianAssistentUI/SpherometrCalculator.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SpherometrCalcLib;
+
+namespace OpticianAssistentUI
+{
+    /// <summary>
+    /// Interaction logic for SpherometrCalculator.xaml
+    /// </summary>
+    public partial class SpherometrCalculator : Window
+    {
+        public SpherometrCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateSpheraParametrs(object sender, RoutedEventArgs e)
+        {
+            message.Text = "Hello, world!";
+        }
+    }
+}
---
+++ SpherometrCalcLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpherometrCalcLib
+{
+    public class Class1
+    {
+        public static string TestMessage()
+        {
+            return "Тестовое сообщение из класса Class1 библиотеки SpherometrCalcLib";
+        }
+    }
+}
