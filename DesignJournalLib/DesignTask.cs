143aa38f89a1da9673a6e8313210fb80ccb5e4ef
konry-ru <konry-ru@yandex.ru>
2021-09-11 13:34:14 +0700

    creation of task DesignTask

---
+++ DesignJournalLib/DesignTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignJournalLib
+{
+    public class DesignTask
+    {
+        static int taskNumber;
+
+        int DesignTaskId { get; set; }
+        string DesignTaskContent { get; set; }
+        DateTime DesignTaskCreationTime { get; set; }
+        DateTime DesignTaskLastUpdateTime { get; set; }
+    }
+}
